375e2312d30e58d22503f4ff5aef31ced053d013
huyenminnn <huyenttm.13@gmail.com>
2019-07-11 14:10:05 +0700

    task_routing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,12 +68,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">{{ __('translate.home') }}</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">{{ __('translate.login') }}</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">{{ __('translate.register') }}</a>
                         @endif
                     @endauth
                 </div>
